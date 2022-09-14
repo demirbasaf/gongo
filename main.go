8117c897f5fc6193d236a9d2b0b44b7e2d6baf91
Ali Furkan Demirbaş <alistefurkan@hotmail.com>
2022-09-14 19:23:07 +0300

    main go removed

--- main.go
+++ main.go
@@ -1,18 +1,3 @@
 package main
 
-import (
-	"fmt"
-	"github.com/afdemirbas/gongo/gongo"
-	"github.com/afdemirbas/gongo/gongo/examples"
-)
-
-func main() {
-
-	userModel := gongo.Model(examples.User{})
-	articleModel := gongo.Model(examples.Article{})
-
-	r := userModel.New("")
-	r2 := articleModel.New("")
-
-	fmt.Println(r, r2)
-}
+func main() {}
